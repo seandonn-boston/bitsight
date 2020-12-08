6db332fb2462ac4d5b1a14da3026da38f94252eb
Sean Donnellan <sean@seandonn.io>
2020-12-08 11:29:48 -0500

    Update fetch logic to handle success, success not found, and catch err

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [topFiveRepos, setTopFiveRepos] = useState({});
-  const [topFiveUsers, setTopFiveUsers] = useState({});
+  const [hotReposData, setHotReposData] = useState({});
+  const [prolificUsersData, setProlificUserData] = useState({});
+  const [errRepo, setErrRepo] = useState(null);
+  const [errUser, setErrUser] = useState(null);
 
   const fetchRepoData = () => {
     fetch(
       "https://api.github.com/search/repositories?q=created:2020-11-01..2020-12-01&sort=stars&order=desc&per_page=5"
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        if (data.message) {
+          setErrRepo(data.message);
+        } else {
+          setErrRepo(null)
+          setHotReposData(data)
+        }
+      })
+      .catch((err) => {
+        setHotReposData({});
+        setErrRepo(err)
+      });
   };
 
   const fetchUserData = () => {
@@ -19,13 +31,23 @@ function App() {
       "https://api.github.com/search/repositories?q=created:2020-11-01..2020-12-01&sort=stars&order=desc&per_page=5"
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        if (data.message) {
+          setErrUser(data.message);
+        } else {
+          setErrUser(null);
+          setProlificUserData(data);
+        }
+      })
+      .catch((err) => {
+        setProlificUserData({});
+        setErrUser(err)
+      });
   };
 
   useEffect(() => {
-    // fetchRepoData();
-    // fetchUserData();
+    fetchRepoData();
+    fetchUserData();
     const interval = setInterval(() => {
       fetchUserData();
     }, 120000);
